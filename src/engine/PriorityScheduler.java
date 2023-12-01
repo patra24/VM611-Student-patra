f1caf6022176fb41bc57bff39316fe5005eb096c
Tim Kington <tim.kington@tanium.com>
2023-12-01 18:24:22 -0500

    hw5 solution

--- src/engine/PriorityScheduler.java
+++ src/engine/PriorityScheduler.java
@@ -1,11 +1,17 @@
 package engine;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Scheduler that runs the highest priority threads first.
  */
 public class PriorityScheduler {
+
+    /** Ready threads, with highest priority at the front. */
+    protected PriorityQueue<VMThread> threads;
+    /** Sleeping threads, with earliest to wake at the front. */
+    protected PriorityQueue<SleepInfo> sleepingThreads;
     /** Current time. */
     private int currentTime;
 
@@ -16,6 +22,11 @@ public class PriorityScheduler {
      */
     public PriorityScheduler(List<VMThread> list) {
         currentTime = 0;
+        threads = new PriorityQueue<VMThread>(10,
+            (o1, o2) -> o2.getPriority() - o1.getPriority());
+
+        threads.addAll(list);
+        sleepingThreads = new PriorityQueue<SleepInfo>();
     }
 
     /**
@@ -24,7 +35,7 @@ public class PriorityScheduler {
      * @return the current thread
      */
     public VMThread getCurrentThread() {
-        return null;
+        return threads.peek();
     }
 
     /**
@@ -36,6 +47,7 @@ public class PriorityScheduler {
         for (int i = 0; i < numInstructions; i++) {
             currentTime++;
             tick();
+            wakeThreads();
         }
     }
 
@@ -43,6 +55,66 @@ public class PriorityScheduler {
      * Executes one instruction.
      */
     private void tick() {
+        if (threads.isEmpty()) {
+            return;
+        }
+
+        VMThread currentThread = threads.peek();
+        VMThreadState state = currentThread.runInstruction();
+
+        switch (state.getState()) {
+        case Running -> {
+            // Do nothing.
+        }
+        case Yielded -> {
+            // Give another thread a chance to run (only if it's of equal priority).
+            threads.add(threads.remove());
+        }
+        case Sleeping -> {
+            int wakeTime = currentTime + state.getTicksToSleep();
+            sleepingThreads.add(new SleepInfo(threads.remove(), wakeTime));
+        }
+        case Complete -> threads.remove();
+        }
+    }
+
+    /**
+     * Find threads that are ready to wake up.
+     */
+    public void wakeThreads() {
+        while (!sleepingThreads.isEmpty() &&
+            sleepingThreads.peek().timeToWake <= currentTime) {
+            threads.add(sleepingThreads.remove().thread);
+        }
+    }
+
+    /**
+     * Remembers how long a thread needs to sleep.
+     */
+    private static class SleepInfo implements Comparable<SleepInfo> {
+        /** The thread. */
+        private VMThread thread;
+        /** Time the thread should wake. */
+        private int timeToWake;
+
+        /**
+         * Creates a SleepInfo.
+         *
+         * @param athread     the thread
+         * @param atimeToWake when the thread should wake
+         */
+        public SleepInfo(VMThread athread, int atimeToWake) {
+            thread = athread;
+            timeToWake = atimeToWake;
+        }
+
+        /**
+         * Comparable implementation.
+         */
+        @Override
+        public int compareTo(SleepInfo o) {
+            return timeToWake - o.timeToWake;
+        }
     }
 
 }
--- test/Part05Test.java
+++ test/Part05Test.java
@@ -1,6 +1,6 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +11,13 @@ import org.junit.Test;
 import engine.PriorityScheduler;
 import engine.VMThread;
 
-public class Part05Test {
+public class Part05Test extends TestBase {
+
+    /** Test execution. */
     @Test
     public void testSimpleExec() {
+        /** #score(6) */
+        hintContext = "(prio)";
         TestUtil.compileMethod("t1", """
             load_const 5
             store_local x
@@ -24,35 +28,392 @@ public class Part05Test {
             """);
         VMThread t1 = new VMThread("t1");
         Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
 
         List<VMThread> threadList = new ArrayList<VMThread>();
         threadList.add(t1);
 
         PriorityScheduler scheduler = new PriorityScheduler(threadList);
 
-        // t1 should be the current thread, and symbols should be empty.
-        assertSame(t1, scheduler.getCurrentThread());
-        assertEquals(0, t1Symbols.size());
+        assertSame(hint("getCurrentThread returns wrong object"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("symbol table should be empty initially"), 0, t1Symbols.size());
 
-        // load_const 5
         scheduler.run(1);
-        assertEquals(0, t1Symbols.size());
+        assertEquals(hint("symbol table should still be empty"), 0, t1Symbols.size());
 
-        // store_const x
         scheduler.run(1);
-        assertEquals(1, t1Symbols.size());
-        assertEquals(5, (int) t1Symbols.get("x"));
+        assertEquals(hint("symbol table contains wrong number of entries"), 1, t1Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
 
-        // load_const 3, load_local x, add
         scheduler.run(3);
-        assertEquals(1, t1Symbols.size());
-        assertSame(t1, scheduler.getCurrentThread());
+        assertEquals(hint("symbol table contains wrong number of entries"), 1, t1Symbols.size());
+        assertSame(hint("getCurrentThread returns wrong object"), t1, scheduler.getCurrentThread());
 
-        // store_local y, thread completes
         scheduler.run(1);
-        assertEquals(2, t1Symbols.size());
-        assertEquals(5, (int) t1Symbols.get("x"));
-        assertEquals(8, (int) t1Symbols.get("y"));
-        assertNull(scheduler.getCurrentThread());
+        assertEquals(hint("symbol table contains wrong number of entries"), 2, t1Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("symbol didn't have expected value"), 8, (int) t1Symbols.get("y"));
+        assertNull(hint("getCurrentThread should return null"), scheduler.getCurrentThread());
     }
+
+    /** Test completion. */
+    @Test
+    public void testCompletion() {
+        /** #score(6) */
+        hintContext = "(prio)";
+
+        TestUtil.compileMethod("t1", """
+            load_const 5
+            store_local x
+            load_const 7
+            store_local y
+            """);
+        VMThread t1 = new VMThread("t1");
+        Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
+
+        TestUtil.compileMethod("t2", """
+            load_const 6
+            store_local z
+            """);
+        VMThread t2 = new VMThread("t2");
+        Map<String, Integer> t2Symbols = t2.getEntryPointLocals();
+        t2Symbols.remove("this");
+
+        List<VMThread> threadList = new ArrayList<VMThread>();
+        threadList.add(t1);
+        threadList.add(t2);
+
+        PriorityScheduler scheduler = new PriorityScheduler(threadList);
+
+        assertSame(hint("getCurrentThread returns wrong object"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table should be empty initially"), 0, t1Symbols.size());
+
+        scheduler.run(3);
+        assertSame(hint("current thread changed prematurely"), t1, scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertEquals(hint("t1 t1 symbol table contains wrong number of entries"), 2, t1Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value after completion"), 5,
+            (int) t1Symbols.get("x"));
+        assertEquals(hint("t1 symbol didn't have expected value after completion"), 7,
+            (int) t1Symbols.get("y"));
+        assertSame(hint("didn't switch to second thread after first completed"), t2, scheduler.getCurrentThread());
+
+        scheduler.run(2);
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t1 t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("t1 symbol didn't have expected value"), 7, (int) t1Symbols.get("y"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+        assertNull(hint("current thread should be null after all threads complete"), scheduler.getCurrentThread());
+    }
+
+    /** Test yield. */
+    @Test
+    public void testYield() {
+        /** #score(6) */
+        hintContext = "(prio)";
+
+        TestUtil.compileMethod("t1", """
+            load_const 5
+            store_local x
+            yield
+            load_const 7
+            store_local y
+            """);
+        VMThread t1 = new VMThread("t1");
+        Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
+
+        TestUtil.compileMethod("t2", """
+            load_const 6
+            store_local z
+            yield
+            """);
+        VMThread t2 = new VMThread("t2");
+        Map<String, Integer> t2Symbols = t2.getEntryPointLocals();
+        t2Symbols.remove("this");
+
+        List<VMThread> threadList = new ArrayList<VMThread>();
+        threadList.add(t1);
+        threadList.add(t2);
+
+        PriorityScheduler scheduler = new PriorityScheduler(threadList);
+
+        assertSame(hint("getCurrentThread returns wrong object"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table should be empty initially"), 0, t1Symbols.size());
+
+        scheduler.run(2);
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 1, t1Symbols.size());
+        assertEquals(hint("t1 t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertSame(hint("current thread changed prematurely"), t1, scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertSame(hint("yield did not change current thread"), t2, scheduler.getCurrentThread());
+
+        scheduler.run(2);
+        assertSame(hint("current thread changed prematurely"), t2, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 1, t1Symbols.size());
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t1 t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+
+        scheduler.run(1);
+        assertSame(hint("yield didn't change back to first thread"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 1, t1Symbols.size());
+
+        scheduler.run(2);
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 2, t1Symbols.size());
+        assertEquals(hint("t1 t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("t1 symbol didn't have expected value"), 7, (int) t1Symbols.get("y"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+    }
+
+    /** Test sleep. */
+    @Test
+    public void testSleep() {
+        /** #score(3) */
+        hintContext = "(prio)";
+        TestUtil.compileMethod("t1", """
+            load_const 5
+            store_local x
+            sleep 1
+            load_const 7
+            store_local y
+            """);
+        VMThread t1 = new VMThread("t1");
+        Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
+
+        TestUtil.compileMethod("t2", """
+            load_const 6
+            store_local z
+            """);
+        VMThread t2 = new VMThread("t2");
+        Map<String, Integer> t2Symbols = t2.getEntryPointLocals();
+        t2Symbols.remove("this");
+
+        List<VMThread> threadList = new ArrayList<VMThread>();
+        threadList.add(t1);
+        threadList.add(t2);
+
+        PriorityScheduler scheduler = new PriorityScheduler(threadList);
+
+        assertSame(hint("getCurrentThread returns wrong object"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table should be empty initially"), 0, t1Symbols.size());
+
+        scheduler.run(3);
+        assertSame(hint("sleep did not change current thread"), t2, scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertSame(hint("current thread changed prematurely when sleep expired"), t2, scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertSame(hint("thread didn't wake up after sleeping"), t1, scheduler.getCurrentThread());
+
+        scheduler.run(2);
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 2, t1Symbols.size());
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("t1 symbol didn't have expected value"), 7, (int) t1Symbols.get("y"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+    }
+
+    /** Test sleep. */
+    @Test
+    public void testSleep2() {
+        /** #score(3) */
+        hintContext = "(prio)";
+        TestUtil.compileMethod("t1", """
+            load_const 5
+            store_local x
+            sleep 3
+            load_const 7
+            store_local y
+            """);
+        VMThread t1 = new VMThread("t1");
+        Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
+
+        TestUtil.compileMethod("t2", """
+            load_const 6
+            store_local z
+            """);
+        VMThread t2 = new VMThread("t2");
+        Map<String, Integer> t2Symbols = t2.getEntryPointLocals();
+        t2Symbols.remove("this");
+
+        List<VMThread> threadList = new ArrayList<VMThread>();
+        threadList.add(t1);
+        threadList.add(t2);
+
+        PriorityScheduler scheduler = new PriorityScheduler(threadList);
+
+        assertSame(hint("getCurrentThread returns wrong object"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table should be empty initially"), 0, t1Symbols.size());
+
+        scheduler.run(3);
+        assertSame(hint("sleep did not change current thread"), t2, scheduler.getCurrentThread());
+
+        scheduler.run(2);
+        assertNull(hint("current thread should be null when all threads are sleeping"), scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertSame(hint("thread didn't wake correctly after sleep"), t1, scheduler.getCurrentThread());
+
+        scheduler.run(2);
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 2, t1Symbols.size());
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value"), 15, (int) t1Symbols.get("x"));
+        assertEquals(hint("t1 symbol didn't have expected value"), 7, (int) t1Symbols.get("y"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+    }
+
+    /** Test sleep. */
+    @Test
+    public void testSleep3() {
+        /** #score(3) */
+        hintContext = "(prio)";
+        TestUtil.compileMethod("t1", """
+            load_const 5
+            sleep 3
+            store_local x
+            """);
+        VMThread t1 = new VMThread("t1");
+        Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
+
+        TestUtil.compileMethod("t2", """
+            load_const 6
+            sleep 2
+            store_local z
+            """);
+        VMThread t2 = new VMThread("t2");
+        Map<String, Integer> t2Symbols = t2.getEntryPointLocals();
+        t2Symbols.remove("this");
+
+        List<VMThread> threadList = new ArrayList<VMThread>();
+        threadList.add(t1);
+        threadList.add(t2);
+
+        PriorityScheduler scheduler = new PriorityScheduler(threadList);
+
+        assertSame(hint("getCurrentThread returns wrong object"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table should be empty initially"), 0, t1Symbols.size());
+
+        scheduler.run(2);
+        assertSame(hint("sleep did not change current thread"), t2, scheduler.getCurrentThread());
+
+        scheduler.run(2);
+        assertNull(hint("there should be no current thread when all threads are sleeping"),
+            scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertSame(hint("thread didn't wake correctly after sleep"), t1, scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertSame(hint("thread didn't wake correctly after sleep"), t2, scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertNull(hint("current thread should be null after threads complete"), scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 1, t1Symbols.size());
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+    }
+
+    /** Test priorities. */
+    @Test
+    public void testPriorities() {
+        /** #score(6) */
+        hintContext = "(prio)";
+        TestUtil.compileMethod("t1", """
+            load_const 5
+            store_local x
+            """);
+        VMThread t1 = new VMThread("t1", 0);
+        Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
+
+        TestUtil.compileMethod("t2", """
+            load_const 6
+            store_local z
+            """);
+        VMThread t2 = new VMThread("t2", 1);
+        Map<String, Integer> t2Symbols = t2.getEntryPointLocals();
+        t2Symbols.remove("this");
+
+        List<VMThread> threadList = new ArrayList<VMThread>();
+        threadList.add(t1);
+        threadList.add(t2);
+
+        PriorityScheduler scheduler = new PriorityScheduler(threadList);
+
+        assertSame(hint("getCurrentThread returns wrong object"), t2, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table should be empty initially"), 0, t1Symbols.size());
+
+        scheduler.run(2);
+        assertSame(hint("current thread wrong after thread ended"), t1, scheduler.getCurrentThread());
+
+        scheduler.run(2);
+        assertNull(hint("current thread should be null"), scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 1, t1Symbols.size());
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+    }
+
+    /** Test priorities. */
+    @Test
+    public void testPriorities2() {
+        /** #score(6) */
+        hintContext = "(prio)";
+
+        TestUtil.compileMethod("t1", """
+            load_const 5
+            store_local x
+            """);
+        VMThread t1 = new VMThread("t1", 0);
+        Map<String, Integer> t1Symbols = t1.getEntryPointLocals();
+        t1Symbols.remove("this");
+
+        TestUtil.compileMethod("t2", """
+            load_const 6
+            sleep 1
+            store_local z
+            """);
+        VMThread t2 = new VMThread("t2", 1);
+        Map<String, Integer> t2Symbols = t2.getEntryPointLocals();
+        t2Symbols.remove("this");
+
+        List<VMThread> threadList = new ArrayList<VMThread>();
+        threadList.add(t1);
+        threadList.add(t2);
+
+        PriorityScheduler scheduler = new PriorityScheduler(threadList);
+
+        assertSame(hint("getCurrentThread returns wrong object"), t2, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table should be empty initially"), 0, t1Symbols.size());
+
+        scheduler.run(2);
+        assertSame(hint("current thread wrong after sleep"), t1, scheduler.getCurrentThread());
+
+        scheduler.run(1);
+        assertSame(hint("high priority thread waking up should have preempted running thread"), t2,
+            scheduler.getCurrentThread());
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 0, t2Symbols.size());
+
+        scheduler.run(1);
+        assertSame(hint("current thread wrong after thread ended"), t1, scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+
+        scheduler.run(1);
+        assertNull(hint("current thread should be null"), scheduler.getCurrentThread());
+        assertEquals(hint("t1 symbol table contains wrong number of entries"), 1, t1Symbols.size());
+        assertEquals(hint("t2 symbol table contains wrong number of entries"), 1, t2Symbols.size());
+        assertEquals(hint("t1 symbol didn't have expected value"), 5, (int) t1Symbols.get("x"));
+        assertEquals(hint("t2 symbol didn't have expected value"), 6, (int) t2Symbols.get("z"));
+    }
+
 }
